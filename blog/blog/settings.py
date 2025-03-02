2b0ec1d5c715c37e103d7643322bd1832361c90c
vino <vinomurugavel@gmail.com>
2025-03-02 16:35:05 +0530

    Fix: Add Render URL to ALLOWED_HOSTS

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8w6ww(4-9f1u#9+32oi-5oq#$ivs$v7k#+05ykdrcwz60gd@l)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
